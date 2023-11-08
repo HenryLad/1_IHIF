75fddbd9ea8a5404f826a05f3d435cd153072f51
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2023-11-08 17:57:12 +0100

    Add Directorys via Upload

---
+++ 1IHIF/Programmieren/Switch Exersice/Program.cs
@@ -0,0 +1,36 @@
+﻿System.Console.Write("Enter your Operator : ");
+char Operator = char.Parse(Console.ReadLine()!);
+System.Console.Write("Enter your first Number :");
+double firstnum = double.Parse(Console.ReadLine()!);
+System.Console.Write("Enter your second Number : ");
+double secondnum = double.Parse(Console.ReadLine()!);
+double result = 0;
+switch(Operator)
+{
+   case '+':
+   result = firstnum + secondnum;
+   break;
+   case '-':
+   result = firstnum - secondnum;
+   break;
+   case '*':
+   result = firstnum * secondnum;
+   break;
+   case '/':
+   if(secondnum != 0){result = firstnum / secondnum;}
+   else{System.Console.WriteLine("You can not devide by zero !");}
+   break;
+   case '%':
+   if(secondnum != 0){result = firstnum % secondnum;}
+   else{System.Console.WriteLine("You can not use Modulo with a zero !");}
+   break;
+   default:
+   System.Console.WriteLine("You entered not a valid Operator !");
+   result = 0;
+   break;
+   
+}
+
+System.Console.WriteLine($"Your result is : {result} ");
+
+
